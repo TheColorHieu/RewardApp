424555023f7e5770cd0b6bd68eb5025f30da94a5
Hieu Tran <hieuthanhtran98@yahoo.com>
2024-02-10 11:28:13 -0700

    trying to add js to the buttons

---
+++ js/signup.js
@@ -0,0 +1,6 @@
+// document.getElementById("signup").addEventListener("click", function() {
+//     document.getElementById("signup").style.display = "none";
+// });
+// document.getElementById("closeSignup").addEventListener("click", function() {
+//     document.getElementById("signup").style.display = "none";
+// });
